3c77e27347901ae6cc71a38171bcdb8ee70ce23c
jinsumin <suminjin@naver.com>
2022-05-16 01:15:19 +0900

    CSS : fontawesome

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,3 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function signIn() {
-    return "sign in page";
+  return (
+    <div className="login-input-section-wrap">
+      <div className="login-input-wrap">
+        <input placeholder="Username" type="text"></input>
+      </div>
+      <div className="login-input-wrap password-wrap">
+        <input placeholder="Password" type="password"></input>
+      </div>
+      <div className="login-button-wrap">
+        <button>Sign in</button>
+      </div>
+      <div className="login-stay-sign-in">
+        <FontAwesomeIcon icon="fa-solid fa-circle-check" />
+      </div>
+    </div>
+  );
 }
