0174b183a3012d3118a59a08ad3073c2ba2df9e1
jinsumin <suminjin@naver.com>
2022-05-15 17:36:13 +0900

    update style : navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,70 +6,64 @@ export default function Navbar() {
 
   return (
     <nav>
-        <div class="navbar_logo">
-            <img src="/netflix.png" href="/"/>
-        </div>
-        <ul class="navbar_menu">
-            <li>
-                <a href="">Home</a></li>
-            <li>
-                <a href="">Photo</a>
-            </li>
-            <li>
-                <a href="">Schedule</a>
-            </li>
-            <li>
-                <a href="">FAQ</a>
-            </li>
-            <li>
-                <a href="">Booking</a>
-            </li>
-        </ul>
-        <ul class="navbar_icons">
-            <li>
-              <i class="fa-brands fa-facebook-square"></i>
-            </li>
-            <li>
-               <i class="fa-brands fa-twitter"></i>
-            </li>
-        </ul>
-        <a href="#" class="navbar_togleBtn">
-            <i class="fa-solid fa-bars"></i>
-        </a>
-
-
       <div>
         <Link href="/">
-          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-        </Link>
-        <Link href="/about">
-          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-        </Link>
-        <Link href="/signIn">
-          <a className={router.pathname === "/signIn" ? "active" : ""}>
-            SignIn
-          </a>
-        </Link>
-        <Link href="/signUp">
-          <a className={router.pathname === "/signUp" ? "active" : ""}>
-            SignUp
+          <a className={router.pathname === "/" ? "active" : ""}>
+            <img src="/netflix.jpeg" />
           </a>
         </Link>
       </div>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Photo</a>
+        </li>
+        <li>
+          <a href="">Schedule</a>
+        </li>
+        <li>
+          <a href="">FAQ</a>
+        </li>
+        <li>
+          <a href="">Booking</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="/signIn">
+            <a className={router.pathname === "/signIn" ? "active" : ""}>
+              SignIn
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/signUp">
+            <a className={router.pathname === "/signUp" ? "active" : ""}>
+              SignUp
+            </a>
+          </Link>
+        </li>
+      </ul>
       <style jsx>{`
         nav {
-          display: flex;
           gap: 10px;
-          flex-direction: column;
+          display: flex;
+          justify-content: space-between;
           align-items: center;
           padding-top: 20px;
           padding-bottom: 10px;
-          box-shadow: rgba(50, 50, 93, 0, 0.25) 0px 50px 100px -20px,
+          padding: 0 2rem;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
             rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        nav div {
+          font-size: 24px;
+          gap: 10px;
         }
         img {
           max-width: 100px;
-          margin-bottom: 5px;
         }
         nav a {
           font-weight: 600;
@@ -78,9 +72,13 @@ export default function Navbar() {
         .active {
           color: tomato;
         }
-        nav div {
+        nav ul {
           display: flex;
-          gap: 10px;
+          list-style: none;
+          padding-left: 0;
+        }
+        nav ul li {
+          padding: 8px 12px;
         }
       `}</style>
     </nav>
