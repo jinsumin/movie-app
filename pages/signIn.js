a7208e1833c59e02639c719b480508844786172b
jinsumin <suminjin@naver.com>
2022-05-16 01:38:19 +0900

    add sign pages

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,20 +1,62 @@
+import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function signIn() {
+  const router = useRouter();
+
   return (
-    <div className="login-input-section-wrap">
-      <div className="login-input-wrap">
-        <input placeholder="Username" type="text"></input>
+    <div>
+      <div className="login-input-section-wrap">
+        <div className="login-input-wrap">
+          <input placeholder="Username" type="text"></input>
+        </div>
+        <div className="login-input-wrap password-wrap">
+          <input placeholder="Password" type="password"></input>
+        </div>
+        <div className="login-button-wrap">
+          <button className="button" onClick={() => router.push("/")}>
+            Sign in
+          </button>
+        </div>
       </div>
-      <div className="login-input-wrap password-wrap">
-        <input placeholder="Password" type="password"></input>
-      </div>
-      <div className="login-button-wrap">
-        <button>Sign in</button>
-      </div>
-      <div className="login-stay-sign-in">
-        <FontAwesomeIcon icon="fa-solid fa-circle-check" />
-      </div>
+      <style jsx>{`
+        .login-input-section-wrap{
+            padding-top: 60px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .login-input-wrap {
+          width: 465px;
+          height: 48px;
+          border: solid 1px var(--border-gray-color);
+          background: white;
+        }
+        .password-wrap {
+          margin-top: 13px;
+        }
+        .login-input-wrap input {
+          border: none;
+          width: 430px;
+          margin-top: 10px;
+          font-size: 14px;
+          margin-left: 10px;
+          height: 30px;
+        }
+        .login-button-wrap {
+          padding-top: 13px;
+          color: black;
+        }
+        .login-button-wrap button {
+          width: 465px;
+          height: 48px;
+          font-size: 18px;
+          background: red;
+          color: white;
+          border: solid 1px red;
+          border-radius: 5px;
+        }
+      `}</style>
     </div>
   );
 }
